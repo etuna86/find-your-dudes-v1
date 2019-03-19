a0b5c80cae49811bb25b8a379bfe9cdb44ca8d95
etuna <erol.tuna@mediatriple.com>
2019-03-19 21:36:06 +0300

    scrollar düzenlendi

--- dist/js.d818e0ef.js
+++ dist/js.d818e0ef.js
@@ -27603,7 +27603,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51815" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64395" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
